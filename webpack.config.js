e1e40ef217b26e15af5b9b83ed06c564f0c35ebf
焦阳 <jiaoyang01@4paradigm.com>
2021-02-23 09:01:24 +0800

    gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
-  entry: [
-    './src/index'
-  ],
+  entry: ["./src/index.js"],
   module: {
     rules: [
-      { test: /\.js?$/, use: ['babel-loader'], exclude: /node_modules/ },
-      { test: /\.s?css$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-    ]
+      { test: /\.js?$/, use: ["babel-loader"], exclude: /node_modules/ },
+      { test: /\.s?css$/, use: ["style-loader", "css-loader", "sass-loader"] },
+    ],
   },
   resolve: {
-    extensions: ['.js','.scss']
+    extensions: [".js", ".scss"],
   },
   output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.join(__dirname, "/dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
   },
-  devtool: 'cheap-eval-source-map',
+  devtool: "cheap-eval-source-map",
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist",
     historyApiFallback: true,
     hot: true,
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
 };
